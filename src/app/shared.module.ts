99440a27ac4500e8c84e8fb82f79f0fd02a31098
Henrique Shodi Maeta <japa1996@hotmail.com>
2017-12-14 23:40:55 -0200

    avanços

---
+++ src/app/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import {  } from '@angular/material';
+
+@NgModule({
+    imports: [
+
+    ],
+    exports: [
+        
+    ]
+})
+export class SharedModule {}
